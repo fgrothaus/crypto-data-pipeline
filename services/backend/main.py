ba11c2618b289dec73700f1e2eac655d387e8973
fgrothaus <felixgrothaus1@gmail.com>
2026-07-11 21:59:12 +0200

    implemented websocket, who provides cached coingecko data

--- services/backend/main.py
+++ services/backend/main.py
@@ -1,13 +1,29 @@
-from fastapi import FastAPI
+import asyncio
+import json
+import os
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from redis import asyncio as aioredis
+from dotenv import load_dotenv
 
+load_dotenv()
 app = FastAPI()
+redis_client = aioredis.from_url(os.getenv("REDIS_URL"), decode_responses=True)
+
+@app.websocket("/ws/prices")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+
+    try:
+        while True:
+            data_string = await redis_client.get("crypto:latest_prices")
+
+            if data_string:
+                await websocket.send_text(data_string)
+            await asyncio.sleep(1)
+    except WebSocketDisconnect:
+        print("Frontend hat die WebSocket-Verbindung getrennt.")
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
