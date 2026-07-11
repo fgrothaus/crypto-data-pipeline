2dcf16755d97b8ecf1c218b80baed188d6527a1d
fgrothaus <felixgrothaus1@gmail.com>
2026-07-11 21:58:16 +0200

    implemented rabbitmq consumer and redis cache

---
+++ services/backend/consumer.py
@@ -0,0 +1,38 @@
+import os
+import asyncio
+import json
+
+from dns.dnssec import validate
+from dotenv import load_dotenv
+from redis import asyncio as aioredis
+from aio_pika import connect, Message, IncomingMessage
+from models import PriceUpdate
+
+load_dotenv()
+redis_client = aioredis.from_url(os.getenv("REDIS_URL"), decode_responses=True)
+
+async def process_message(message: IncomingMessage):
+    async with message.process():
+        try:
+            valid_payload = PriceUpdate.model_validate_json(message.body)
+            json_string = valid_payload.model_dump_json()
+            await redis_client.set("crypto:latest_prices", json_string)
+            print("Actualisation of crypto prices in redis successful")
+        except Exception as e:
+            print(f"Validation failed: {e}")
+
+
+async def start_listening():
+    connection = await connect(os.getenv("RABBITMQ_CONNECTION_STRING"))
+    channel = await connection.channel()
+    queue = await channel.declare_queue("crypto_prices", durable=True)
+
+    await queue.consume(process_message)
+    try:
+        await asyncio.Future()  # Hält den Prozess am Leben
+    finally:
+        await connection.close()
+        await redis_client.aclose()
+
+if __name__ == "__main__":
+    asyncio.run(start_listening())
