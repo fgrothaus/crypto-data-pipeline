b1667dfab7ee9c8918cc8183593508693d8ed795
fgrothaus <felixgrothaus1@gmail.com>
2026-08-02 19:48:18 +0200

    add model for sqlalchemy

--- services/backend/models.py
+++ services/backend/models.py
@@ -1,4 +1,9 @@
 from pydantic import BaseModel
+from datetime import datetime, timezone
+from sqlalchemy import Column, DateTime, Numeric, String, Integer
+
+from database.base import Base
+
 
 class CoinMetrics(BaseModel):
     eur: float
@@ -6,3 +11,13 @@ class CoinMetrics(BaseModel):
 
 class PriceUpdate(BaseModel):
     coins: dict[str, CoinMetrics]
+
+class CryptoPriceDB(Base):
+    __tablename__ = "crypto_price_history"
+
+    id = Column(Integer, primary_key=True, index=True)
+    coin_id = Column(String(50), index=True, nullable=False)
+    symbol = Column(String(10), nullable=False)
+    name = Column(String(100), nullable=False)
+    price_eur = Column(Numeric(18, 8), nullable=False)
+    timestamp = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), index=True)
