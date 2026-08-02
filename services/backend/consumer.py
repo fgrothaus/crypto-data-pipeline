e543eb8f2fc0e5538284b0af67e85f3ee23d756e
fgrothaus <felixgrothaus1@gmail.com>
2026-08-02 19:52:07 +0200

    add save_price_update call, and cleanup logik

--- services/backend/consumer.py
+++ services/backend/consumer.py
@@ -8,6 +8,7 @@ from redis import asyncio as aioredis
 from redis.exceptions import RedisError
 
 from models import PriceUpdate
+from database.database import init_db, save_price_update, cleanup_old_prices
 
 load_dotenv()
 redis_client = aioredis.from_url(os.getenv("REDIS_URL"), decode_responses=True)
@@ -18,12 +19,23 @@ async def process_message(message: IncomingMessage):
             valid_payload = PriceUpdate.model_validate_json(message.body)
             json_string = valid_payload.model_dump_json()
             await redis_client.set("crypto:latest_prices", json_string)
-            # HIER in DB schreiben
             print("Actualisation of crypto prices in redis successful")
+            await save_price_update(valid_payload)
+            print("Saved price update to Postgres!")
         except ValidationError as e:
             print(f"Invalid message format, skipping: {e}")
         except RedisError as e:
             print(f"Redis write failed: {e}")
+
+
+async def cleanup_loop():
+    while True:
+        try:
+            # Alle 60 Sekunden prüfen und ggf. aufräumen
+            await asyncio.sleep(60)
+            await cleanup_old_prices(max_records=8000)
+        except Exception as e:
+            print(f"Error during DB cleanup: {e}")
 
 
 async def start_listening():
@@ -32,10 +44,13 @@ async def start_listening():
         try:
             connection = await connect(os.getenv("RABBITMQ_CONNECTION_STRING"))
             print("Successfully connected to RabbitMQ!")
+            await init_db()
             break
         except Exception as e:
             print(f"RabbitMQ not ready yet ({e}), retrying in 3 seconds...")
             await asyncio.sleep(3)
+
+    asyncio.create_task(cleanup_loop())
 
     channel = await connection.channel()
     queue = await channel.declare_queue("crypto_prices", durable=True)
